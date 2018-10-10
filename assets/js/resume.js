27424d4162b03cdbb6335b0756ce26c854ca570b
Cu7ious <328@holbertonschool.com>
2018-10-10 23:05:34 +0000

    Fixed minor bugs like broken link

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,5 +1,5 @@
 window.onload = () => {
   // @TODO: make it editable only for myself via special command from the console
-  // const elements = document.querySelector('.doc');
-  // const editor = new MediumEditor(elements);
+  const elements = document.querySelector('.doc');
+  const editor = new MediumEditor(elements);
 }
