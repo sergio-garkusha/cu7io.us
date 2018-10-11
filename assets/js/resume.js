fe05a283432d9072152179c655422452c97fa648
Cu7ious <328@holbertonschool.com>
2018-10-11 02:57:45 +0000

    Added 'Print as PDF feature'

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,5 +1,12 @@
 window.onload = () => {
+  const printButton = document.getElementById('print-as-pdf');
+
+  // Print button
+  printButton.addEventListener('click', e => {
+    window.print();
+  }, false);
+
   // @TODO: make it editable only for myself via special command from the console
-  const elements = document.querySelector('.doc');
-  const editor = new MediumEditor(elements);
+  // const elements = document.querySelector('.doc');
+  // const editor = new MediumEditor(elements);
 }
