5144641b21b6f8b58db62a7a395d32fcf20bbf69
Sergii Garkusha <garkusha.sergiy@gmail.com>
2020-09-09 22:40:04 -0700

    Added verbal description of the steps

--- temp_dep.sh
+++ temp_dep.sh
@@ -2,12 +2,15 @@
 FILE=${PWD}/package.json
 
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "Cleaning up the mess"
     rm -rf .git .gitignore Gruntfile.js dev-server.sh package.json README.md
+    echo "Folder is in production-ready state"
 else 
-    echo "$FILE does not exist."
+    echo "Pulling remote updates"
+    rm -rf assets
     git clone https://github.com/Cu7ious/cu7io.us.git && cd cu7io.us && mv * .. && cd .. && rm -rf cu7io.us
     chmod 700 temp_dep.sh
     chmod -R 650 assets
     chmod 440 index.html resume.html
+    echo "Updates are pulled. Run me one more time to clean the mess :)"
 fi
