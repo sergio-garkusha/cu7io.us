e96b760ff47b22b0889c881a9b0a58a31922000b
Sergii Garkusha <garkusha.sergiy@gmail.com>
2020-09-09 22:34:53 -0700

    Added proper permissions

--- temp_dep.sh
+++ temp_dep.sh
@@ -7,4 +7,7 @@ if [ -f "$FILE" ]; then
 else 
     echo "$FILE does not exist."
     git clone https://github.com/Cu7ious/cu7io.us.git && cd cu7io.us && mv * .. && cd .. && rm -rf cu7io.us
+    chmod 700 temp_dep.sh
+    chmod -R 650 assets
+    chmod 440 index.html resume.html
 fi
