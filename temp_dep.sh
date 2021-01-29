ab2865ea5d9c9fbcc5272c23855dda713d41ec48
Sergii Garkusha <garkusha.sergiy@gmail.com>
2021-01-28 19:05:01 -0800

    Update temp_dep.sh
    Testing the GH Actions

--- temp_dep.sh
+++ temp_dep.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
-FILE=${PWD}/package.json
 
-if [ -f "$FILE" ]; then
-    echo "Cleaning up the mess"
-    rm -rf .git .gitignore Gruntfile.js dev-server.sh package.json README.md
-    echo "Folder is in production-ready state"
-else 
-    echo "Pulling remote updates"
-    rm -rf assets
-    git clone https://github.com/Cu7ious/cu7io.us.git && cd cu7io.us && mv * .. && cd .. && rm -rf cu7io.us
-    chmod 700 temp_dep.sh
-    chmod -R 650 assets
-    chmod 440 index.html resume.html
-    echo "Updates are pulled. Run me one more time to clean the mess :)"
-fi
+mkdir dist && touch dist/index.html && echo "test" > dist/index.html
