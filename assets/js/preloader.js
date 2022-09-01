1b3612c084a03d5c46e0ec3643f9f53c3484dcc4
Sergii Garkusha <garkusha.sergiy@gmail.com>
2022-09-01 00:41:36 -0700

    Fundamental fix of mobile responsiveness; Scrolling works on Safari; Added Support Ukraine banner; Applied Blue and Yellow color pallette

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,17 +1,18 @@
 // Preloader
 window.onload = function () {
-  var preload = document.getElementById('preloader-stage');
-  var header = document.getElementById('header');
-  var headerImage = document.getElementById('headerImage');
-  var src = headerImage.getAttribute('src');
+  setTimeout(() => {
+    const preload = document.getElementById('preloader-stage');
+    // const header = document.getElementById('header');
+    // const headerImage = document.getElementById('headerImage');
+    // const src = headerImage.getAttribute('src');
 
-  header.style.backgroundImage = 'url(' + window.location.origin + '/' + src + ')';
-  headerImage.remove();
+    // header.style.backgroundImage = 'url(' + window.location.origin + '/' + src + ')';
+    // headerImage.remove();
 
-  preload.style.opacity = 0;
-  setTimeout(function () {
-    preload.remove();
-  }, 1200);
-
+    preload.style.opacity = 0;
+    setTimeout(function () {
+      preload.remove();
+    }, 1200);
+  }, 1000);
 };
 // Preloader END
